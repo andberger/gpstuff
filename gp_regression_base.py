f3e34564110b2146e7edb1648c164209959c3bd0
andberger <and.bergsson@gmail.com>
2018-04-02 21:18:38 +0200

    attempting to setup a gaussian elimination algorithm, work in progress

--- gp_regression_base.py
+++ gp_regression_base.py
@@ -2,12 +2,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 from scipy.spatial.distance import cdist
+from itertools import chain
 #import kernel_functions as kf
 
 def kernel_wp_nonce_local(x, y, sigma):
     minimum = cdist(x.reshape((-1, 1)), y.reshape((-1, 1)), lambda u, v: np.fmin(u,v))
     return (sigma**2) * minimum
     
+def inversion_algo(K):
+    #identity = np.eye(len(K))
+    
+    # Gaussian elimination on K
+    #gaussian_elimination(K)
+    
+    # Same Gaussian elimination on identity
+    #gaussian_elimination(identity)
+    
+    #return identity
+    
+    return np.linalg.pinv(K)
+
+def gaussian_elimination(A):
+    h = 1
+    k = 1
+    m = len(A)
+    n = len(A)
+    while h <= m and k <= n:
+        i_max = max(list(chain.from_iterable((i, abs(A[i,k-1])) for i in xrange(1, m))))
+        if A[i_max, k-1] == 0:
+            k = k+1
+        else:
+            swap_rows(h, i_max, A)
+            for i in range(h+1, m):
+                f = A[i, k] / A[h, k]
+                A[i, k] = 0
+                for j in range(k+1,n):
+                    A[i, j] = A[i, j] - A[k, j] * f
+            h = h+1
+            k = k+1
+            
+def swap_rows(i1, i2, A):
+    A[i1], A[i2] = A[i2], A[i1]
+            
+tt = np.matrix([[1,2,3],[4,5,6],[7,8,9]])
+gaussian_elimination(tt)
 ## Load data
 ## We subsample the data, which gives us N pairs of (x, y)
 data = loadmat('weather.mat')
@@ -32,15 +70,9 @@ sigma2 = (1.0)**2
 hyper_parameter = 1.0
 
 ## Compute covariance (aka "kernel") matrices
-K = kernel_wp_nonce_local(x, x, hyper_parameter) + sigma2*np.eye(N) 
+K = kernel_wp_nonce_local(x, x, hyper_parameter) + sigma2*np.eye(N)
 Ks = kernel_wp_nonce_local(x, xs, hyper_parameter)
 Kss = kernel_wp_nonce_local(xs, xs, hyper_parameter)
-    
-def inversion_algo(K):
-    identity = np.eye(len(K))
-    # Gaussian elimination on K
-    
-    # Same Gaussian elimination on identity
  
 ## Compute conditional mean p(y_* | x, y, x_*)
 Kinv = np.linalg.pinv(K)
