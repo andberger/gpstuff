3564a68bd57b33182c041b32edd24ed05c9eb679
andberger <and.bergsson@gmail.com>
2018-05-21 14:20:35 +0200

    finishing touches to kernel_functions.py

--- kernel_functions.py
+++ kernel_functions.py
@@ -4,33 +4,31 @@ from scipy.spatial.distance import cdist
 
 def kernel_wp_nonce(x, y, sigma):
     minimum = cdist(x.reshape((-1, 1)), y.reshape((-1, 1)), lambda u, v: np.fmin(u,v))
-    return sigma * minimum
+    return (sigma**2) * minimum
 
 def kernel_wp_once(x, y, sigma):
     dist = cdist(x.reshape((-1, 1)), y.reshape((-1, 1)), 'euclidean')
     minimum = cdist(x.reshape((-1, 1)), y.reshape((-1, 1)), lambda u, v: np.fmin(u,v))
     term1 = np.power(minimum, 3) / 3
-    term2 = np.power(minimum,2) / 2
+    term2 = np.power(minimum, 2) / 2
     
-    return sigma * (term1 + dist*term2)
+    return (sigma**2) * (term1 + dist*term2)
 
-def kernel_wp_once2(x, y, sigma):
+def kernel_wp_twice(x, y, sigma):
+    dist = cdist(x.reshape((-1, 1)), y.reshape((-1, 1)), 'euclidean')
+    minimum = cdist(x.reshape((-1, 1)), y.reshape((-1, 1)), lambda u, v: np.fmin(u,v))
+    term1 = np.power(minimum, 5) / 20
+    term2 = np.power(minimum, 3)
+    term3 = np.power(minimum, 4) / 2
+    
+    return (sigma**2) * (term1 + (dist/12) * ((x + y) * term2 - term3))
 
-    # x er vigur 1xN
-    # y er vigur 1xM
-    # sigma er rauntala
-    # viljum skila samfylgnifylki
-    N = len(x)
-    M = len(y)
-    covmat = np.zeros((N, M))
-    for i in range(N):
-        a = x[i]
-        for j in range(M):
-            b = y[j]
-            d1 = np.power(np.fmin(a, b), 3)/3
-            d2 = np.power(np.fmin(a, b), 2)/2
-            v = sigma*sigma*(d1 + np.abs(a-b) * d2)
-            #print(N, M, i, j, v)
-            covmat[i,j] = v
-            
-    return covmat
+def kernel_wp_thrice(x, y, sigma):
+    dist = cdist(x.reshape((-1, 1)), y.reshape((-1, 1)), 'euclidean')
+    minimum = cdist(x.reshape((-1, 1)), y.reshape((-1, 1)), lambda u, v: np.fmin(u,v))
+    maximum = cdist(x.reshape((-1, 1)), y.reshape((-1, 1)), lambda u, v: np.fmax(u,v))
+    term1 = np.power(minimum, 7) / 252
+    term2 = np.power(minimum, 4)
+    term3 = np.power(minimum, 2)
+    
+    return (sigma**2) * (term1 + (dist*term2)/720 * (5*np.power(maximum, 2) + 2*x*y + 3*term3))
